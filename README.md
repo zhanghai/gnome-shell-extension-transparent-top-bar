618ce38c5337fc7c2f236125d6aeaa480f261fc0
Zhang Hai <dreaming.in.code.zh@gmail.com>
2019-03-22 01:34:41 -0700

    [Feature] Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# GNOME Shell Extension - Transparent Top Bar
+
+A GNOME Shell extension that makes the top bar transparent when free-floating.
+
+This is basically a copy of the feature implementation [removed in GNOME Shell 3.32](https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/376/). I modified the code a bit to make it an extension.
+
+Enjoy!
+
+## License
+
+This program is distributed under the terms of the GNU General Public License, version 2 or later.
---
+++ extension.js
@@ -0,0 +1,124 @@
+const Meta = imports.gi.Meta;
+const St = imports.gi.St;
+
+const Main = imports.ui.main;
+
+var Extension = class Extension {
+
+    constructor() {
+        this._actorSignalIds = null;
+        this._windowSignalIds = null;
+    }
+
+    enable() {
+        this._actorSignalIds = new Map();
+        this._windowSignalIds = new Map();
+
+        this._actorSignalIds.set(Main.overview, [
+            Main.overview.connect('showing', this._updateTransparent.bind(this)),
+            Main.overview.connect('hiding', this._updateTransparent.bind(this))
+        ]);
+
+        this._actorSignalIds.set(Main.sessionMode, [
+            Main.sessionMode.connect('updated', this._updateTransparent.bind(this))
+        ]);
+
+        this._actorSignalIds.set(global.window_group, [
+            global.window_group.connect('actor-added', this._onWindowActorAdded.bind(this)),
+            global.window_group.connect('actor-removed', this._onWindowActorRemoved.bind(this))
+        ]);
+
+        this._actorSignalIds.set(global.window_manager, [
+            global.window_manager.connect('switch-workspace', this._updateTransparent.bind(this))
+        ]);
+
+        this._updateTransparent();
+    }
+
+    disable() {
+        for (const actorSignalIds of [this._actorSignalIds, this._windowSignalIds]) {
+            for (const [actor, signalIds] of actorSignalIds) {
+                for (const signalId of signalIds) {
+                    actor.disconnect(signalId);
+                }
+            }
+        }
+        this._actorSignalIds = null;
+        this._windowSignalIds = null;
+
+        Main.panel.remove_style_class_name('transparent-top-bar--solid');
+        Main.panel.remove_style_class_name('transparent-top-bar--transparent');
+    }
+
+    _onWindowActorAdded(container, metaWindowActor) {
+        this._windowSignalIds.set(metaWindowActor, [
+            metaWindowActor.connect('allocation-changed', this._updateTransparent.bind(this)),
+            metaWindowActor.connect('notify::visible', this._updateTransparent.bind(this))
+        ]);
+    }
+
+    _onWindowActorRemoved(container, metaWindowActor) {
+        for (const signalId of this._windowSignalIds.get(metaWindowActor)) {
+            metaWindowActor.disconnect(signalId);
+        }
+        this._windowSignalIds.delete(metaWindowActor);
+        this._updateTransparent();
+    }
+
+    _updateTransparent() {
+
+        if (Main.panel.has_style_pseudo_class('overview') || !Main.sessionMode.hasWindows) {
+            this._setTransparent(true);
+            return;
+        }
+
+        if (!Main.layoutManager.primaryMonitor) {
+            return;
+        }
+
+        // Get all the windows in the active workspace that are in the primary monitor and visible.
+        const workspaceManager = global.workspace_manager;
+        const activeWorkspace = workspaceManager.get_active_workspace();
+        const windows = activeWorkspace.list_windows().filter(metaWindow => {
+            return metaWindow.is_on_primary_monitor()
+                    && metaWindow.showing_on_its_workspace()
+                    && !metaWindow.is_hidden()
+                    && metaWindow.get_window_type() !== Meta.WindowType.DESKTOP;
+        });
+
+        // Check if at least one window is near enough to the panel.
+        const panelTop = Main.panel.get_transformed_position()[1];
+        const panelBottom = panelTop + Main.panel.get_height();
+        const scale = St.ThemeContext.get_for_stage(global.stage).scale_factor;
+        const isNearEnough = windows.some(metaWindow => {
+            const verticalPosition = metaWindow.get_frame_rect().y;
+            return verticalPosition < panelBottom + 5 * scale;
+        });
+
+        this._setTransparent(!isNearEnough);
+    }
+
+    _setTransparent(transparent) {
+        if (transparent) {
+            Main.panel.remove_style_class_name('transparent-top-bar--solid');
+            Main.panel.add_style_class_name('transparent-top-bar--transparent');
+        } else {
+            Main.panel.add_style_class_name('transparent-top-bar--solid');
+            Main.panel.remove_style_class_name('transparent-top-bar--transparent');
+        }
+    }
+};
+
+let extension;
+
+function init() {
+    extension = new Extension();
+}
+
+function enable() {
+    extension.enable();
+}
+
+function disable() {
+    extension.disable();
+}
