38fe14c04e65d94b6b5157a68afa9483dafefc34
Paul Fauchon <p.fauchon@gmail.com>
2021-04-11 12:00:56 +0800

    make extension work for gnome 40

--- src/prefs.js
+++ src/prefs.js
@@ -3,14 +3,9 @@ const Gtk = imports.gi.Gtk;
 const Gio = imports.gi.Gio;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 
-function init() {
-}
-
-function buildPrefsWidget() {
-    let widget = new MyPrefsWidget();
-    widget.show_all();
-    return widget;
-}
+const Config = imports.misc.config;
+const [major] = Config.PACKAGE_VERSION.split('.');
+const shellVersion = Number.parseInt(major);
 
 function getSettings() {
     let GioSSS = Gio.SettingsSchemaSource;
@@ -27,39 +22,77 @@ function getSettings() {
     return new Gio.Settings({settings_schema: schemaObj});
 }
 
-const MyPrefsWidget = GObject.registerClass(
-    class MyPrefsWidget extends Gtk.Box {
+// legacy, gnome 3.38
+if (shellVersion < 40) {
+    function init() {
+    }
 
-        _init(params) {
+    function buildPrefsWidget() {
+        let widget = new MyPrefsWidget();
+        widget.show_all();
+        return widget;
+    }
 
+    const MyPrefsWidget = GObject.registerClass(
+        class MyPrefsWidget extends Gtk.Box {
+
+            _init(params) {
+                super._init(params);
+
+                this.margin = 20;
+                this.set_spacing(15);
+                this.set_orientation(Gtk.Orientation.VERTICAL);
+                this.settings = getSettings();
+
+                this.connect('destroy', Gtk.main_quit);
+
+                let myLabel = new Gtk.Label({
+                    label: "Top bar opacity (%)"
+                });
+
+                let scale = Gtk.Scale.new_with_range(Gtk.Orientation.HORIZONTAL, 0.0, 100.0, 1.0);
+                scale.set_value(this.settings.get_int("transparency"));
+
+                const _this = this;
+                scale.connect("value-changed", function (w) {
+                    _this.settings.set_int("transparency", w.get_value());
+                });
+
+                let box = new Gtk.Box();
+                box.set_orientation(Gtk.Orientation.VERTICAL);
+
+                box.pack_start(myLabel, false, false, 0);
+                box.pack_end(scale, false, false, 0);
+
+                this.add(box);
+            }
+
+        });
+} else { // gnome 40
+    const PrefsWidget = GObject.registerClass({
+        GTypeName: 'PrefsWidget',
+        Template: Me.dir.get_child('prefs.gtk4.ui').get_uri(),
+        InternalChildren: [
+            'opacity'
+        ]
+    }, class PrefsWidget extends Gtk.Box {
+
+        _init(params = {}) {
             super._init(params);
-
-            this.margin = 20;
-            this.set_spacing(15);
-            this.set_orientation(Gtk.Orientation.VERTICAL);
             this.settings = getSettings();
 
-            this.connect('destroy', Gtk.main_quit);
-
-            let myLabel = new Gtk.Label({
-                label: "Top bar transparency (%)"
-            });
-
-            let scale = Gtk.Scale.new_with_range(Gtk.Orientation.HORIZONTAL, 0.0, 100.0, 1.0);
-            scale.set_value(this.settings.get_int("transparency"));
-
-            const _this = this;
-            scale.connect("value-changed", function (w) {
-                _this.settings.set_int("transparency", w.get_value());
-            });
-
-            let box = new Gtk.Box();
-            box.set_orientation(Gtk.Orientation.VERTICAL);
-
-            box.pack_start(myLabel, false, false, 0);
-            box.pack_end(scale, false, false, 0);
-
-            this.add(box);
+            this._opacity.set_value(this.settings.get_int("transparency"));
         }
 
+        onValueChanged(scale) {
+            this.settings.set_int("transparency", this._opacity.get_value());
+        }
     });
+
+    function init() {
+    }
+
+    function buildPrefsWidget() {
+        return new PrefsWidget();
+    }
+}
